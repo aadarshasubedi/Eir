6ae31f2f6276dd50fbbdda8ceef58d33239480e3
cbrenon <christophe.brenon@gmail.com>
2015-11-23 13:50:29 +0100

    commit with auto script

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,9 +1,8 @@
 #!/bin/zsh
 
-cd ..
-git add sources/*
-git add scripts/*
-git add misc/*
-git add resources/*
+git add ../sources/*
+git add ../scripts/*
+git add ../misc/*
+git add ../resources/*
 git commit -m "commit with auto script"
 git push
