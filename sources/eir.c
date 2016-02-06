7ae9ec4ee52bf60527ff50167d4513950df116e8
cbrenon <christophe.brenon@gmail.com>
2016-02-06 12:32:38 +0100

    [FIX][DONE] remove gravity and jump for top down action proto and redo y move

--- sources/eir.c
+++ sources/eir.c
@@ -2,9 +2,9 @@
  *
  * TASKS:
  *
- *    - set a max height for jump
- *		- create state jump_down triggered when player reached max height or collide with another physical object
- *
+ *		- implement based phyisical attack with global cooldown
+ *		- implement navigation zone
+ *		- fix indentation in eir.c
  *    - increase performance by update only modified batch (currently all batch are updated)
  *    - add real texture font manager to stock texture size and cell size anywhere else than in the add_text function
  *		- change add_text return type and update_text parameter type to manipulate created text
@@ -46,12 +46,11 @@
 
 #include "physics/eir_motion_func.h"
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
-#define PLAYER_FRICTION 10.0f
-#define PLAYER_SPEED 1600.0f
-#define PLAYER_GRAVITY 1.0f
-#define PLAYER_JUMP_SPEED -2.0f
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600; 
+
+const float PLAYER_FRICTION = 10.0f;
+const float PLAYER_SPEED = 1600.0f;
 
 typedef struct
 {
@@ -121,6 +120,7 @@ static void eir_set_free_func(eir_free_t free_func)
 
 static void eir_init_env(eir_ker_env_t * env, int width, int height)
 {
+   eir_check_allocate_and_free_func();
    if (env)
    {
       eir_init_all_api(env, width, height);
@@ -265,8 +265,8 @@ static bool validate_idle_state(void * user_data)
 	    && !player->pad_buffer->controllers[1].is_connected
 	    && !player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_RIGHT_BUTTON_INDEX].pressed
 	    && !player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_LEFT_BUTTON_INDEX].pressed
-	    //&& !player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
-	    //&& !player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
+		 && !player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
+	    && !player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
 	    )
 	 {
 	    result = true;
@@ -276,8 +276,8 @@ static bool validate_idle_state(void * user_data)
 	    && player->pad_buffer->controllers[1].is_connected
 	    && !player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_RIGHT_BUTTON_INDEX].pressed
 	    && !player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_LEFT_BUTTON_INDEX].pressed
-	    //&& !player->pad_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
-	    //&& !player->pad_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
+	    && !player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
+	    && !player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
 	    )
 	 {
 	    result = true;
@@ -302,8 +302,8 @@ static bool validate_move_state(void * user_data)
 	 && (
 	    player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_RIGHT_BUTTON_INDEX].pressed
 	    || player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_LEFT_BUTTON_INDEX].pressed
-	    //|| player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
-	    //|| player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
+	    || player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
+	    || player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
 	    )
 	 )
       {
@@ -316,8 +316,8 @@ static bool validate_move_state(void * user_data)
 	 && (
 	    player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_RIGHT_BUTTON_INDEX].pressed
 	    || player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_LEFT_BUTTON_INDEX].pressed
-	    //|| player->pad_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
-	    //|| player->pad_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
+	    || player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
+	    || player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
 	    )
 	 )
       {
@@ -329,30 +329,11 @@ static bool validate_move_state(void * user_data)
 	 && player->pad_buffer->controllers[1].is_analog
 	 && (
 	    player->pad_buffer->controllers[1].left_stick_value_x
-	    //|| player->pad_buffer->controllers[1].left_stick_value_y
+	    || player->pad_buffer->controllers[1].left_stick_value_y
 	    )
 	 )
       {
 	 result = true;
-      }
-   }
-   return result;
-}
-
-static bool validate_jump_up_state(void * user_data)
-{
-   bool result = false;
-
-   if (user_data)
-   {
-      player_t * player = (player_t *)user_data;
-
-      if (
-            player->keyboard_buffer
-            && player->keyboard_buffer->controllers[1].buttons[EIR_GME_JUMP_BUTTON_INDEX].pressed
-         )
-      {
-         result = true;
       }
    }
    return result;
@@ -368,7 +349,7 @@ static void update_idle_state(void * user_data)
 	 player->owner,
 	 player->entity,
 	 0.0f,
-	 PLAYER_GRAVITY, 
+	 0.0f, 
 	 PLAYER_SPEED,
 	 PLAYER_FRICTION
 	 );
@@ -382,11 +363,12 @@ static void update_move_state(void * user_data)
       player_t * player = (player_t *)user_data;
 
       float x_velocity = 0.0f;
+      float y_velocity = 0.0f;
 
       if (player->pad_buffer->controllers[1].is_analog)
       {
 	 x_velocity = player->pad_buffer->controllers[1].left_stick_value_x;
-	 //y_velocity = player->pad_buffer->controllers[1].left_stick_value_y;
+	 y_velocity = player->pad_buffer->controllers[1].left_stick_value_y;
       }
       else
       {
@@ -404,54 +386,33 @@ static void update_move_state(void * user_data)
 	 {
 	    x_velocity = -1.0f;
 	 }
-         /*
 	 if (
-	    player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
-	    || player->pad_buffer->controllers[1].buttons[EIR_MOVE_DOWN_BUTTON_INDEX].pressed
+	    player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
+	    || player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_DOWN_BUTTON_INDEX].pressed
 	    )
 	 {
 	    y_velocity = 1.0f;
 	 }
 	 if (
-	    player->keyboard_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
-	    || player->pad_buffer->controllers[1].buttons[EIR_MOVE_UP_BUTTON_INDEX].pressed
+	    player->keyboard_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
+	    || player->pad_buffer->controllers[1].buttons[EIR_GME_MOVE_UP_BUTTON_INDEX].pressed
 	    )
 	 {
 	    y_velocity = -1.0f;
 	 }
-         */
       }
 
-      /*
       if (eir_mth_abs(x_velocity) > 0.0f && eir_mth_abs(y_velocity) > 0.0f)
       {
 	 x_velocity *= 0.707107f;
 	 y_velocity *= 0.707107f;
       }
-      */
 
       eir_gme_set_entity_acceleration(
 	 player->owner,
 	 player->entity,
 	 x_velocity,
-	 PLAYER_GRAVITY,
-	 PLAYER_SPEED,
-	 PLAYER_FRICTION
-	 );
-   }
-}
-
-static void update_jump_up_state(void * user_data)
-{
-   if (user_data)
-   {
-      player_t * player = (player_t *)user_data;
-
-      eir_gme_set_entity_acceleration(
-	 player->owner,
-	 player->entity,
-	 0.0f,
-	 PLAYER_JUMP_SPEED, 
+	 y_velocity,
 	 PLAYER_SPEED,
 	 PLAYER_FRICTION
 	 );
@@ -460,8 +421,6 @@ static void update_jump_up_state(void * user_data)
 
 int main()
 {
-   eir_check_allocate_and_free_func();
-   
    // INIT ENV
 
    eir_ker_env_t env;
@@ -489,7 +448,7 @@ int main()
    eir_gme_world_t * world = eir_gme_create_world(gme_env, 10);
    eir_gme_entity_t entity = eir_gme_create_world_entity(world);
 
-   eir_gme_set_entity_position(world, entity, 400, 300);
+   eir_gme_position_component_t * position_cpt = eir_gme_set_entity_position(world, entity, 400, 300);
    eir_gme_set_entity_size(world, entity, 64, 64);
    eir_gme_set_entity_sprite_ref(world, entity, ph_sprite_ref);
    eir_gme_set_entity_color(world, entity, 0.0f, 1.0f, 0.0f, 0.5f);
@@ -533,29 +492,22 @@ int main()
 
    eir_fsm_set_state_machine_capacity(fsm_env, 1);
 
-   eir_fsm_state_machine_t * fsm = eir_fsm_create_state_machine(fsm_env, 4, &game.player);
+   eir_fsm_state_machine_t * fsm = eir_fsm_create_state_machine(fsm_env, 3, &game.player);
    eir_fsm_state_t * idle_state = eir_fsm_create_state(fsm);
    eir_fsm_state_t * move_state = eir_fsm_create_state(fsm);
    eir_fsm_state_t * end_state = eir_fsm_create_state(fsm);
-   eir_fsm_state_t * jump_up_state = eir_fsm_create_state(fsm);
 
    eir_fsm_set_begin_state(fsm, idle_state);
    eir_fsm_set_end_state(fsm, end_state);
    
    eir_fsm_set_state_validate_func(idle_state, validate_idle_state);
    eir_fsm_set_state_validate_func(move_state, validate_move_state);
-   eir_fsm_set_state_validate_func(jump_up_state, validate_jump_up_state);
 
    eir_fsm_set_state_update_func(idle_state, update_idle_state);
    eir_fsm_set_state_update_func(move_state, update_move_state);
-   eir_fsm_set_state_update_func(jump_up_state, update_jump_up_state);
 
    eir_fsm_add_state_transition(idle_state, move_state);
-   eir_fsm_add_state_transition(idle_state, jump_up_state);
    eir_fsm_add_state_transition(move_state, idle_state);
-   eir_fsm_add_state_transition(move_state, jump_up_state);
-   eir_fsm_add_state_transition(jump_up_state, idle_state);
-   eir_fsm_add_state_transition(jump_up_state, move_state);
 
    game.player.fsm = fsm;
 
